3000cca6ad7acdcaa3d73d0331ec9a7ce1c87a8d
៚ᴍʀͥ.ᴀͣᴍͫɪʏᴀ༒ᴏꜰᴄ͢⁸⁵⁴¹༆࿐ <techwithamee@gmail.com>
2023-07-29 22:57:52 +0530

    ᴅʀᴀɢᴏɴ-ᴍᴅ ⦁ ᴍᴀᴅᴇ ʙʏ ᴀᴍᴇᴇꜱʜᴀ

--- ext/data.js
+++ ext/data.js
@@ -3,5 +3,8 @@
   "ssapi": "H70C4EW-9PW45AZ-NBJP637-PD5GGFM",
   "latam": "nekosmic",
   "openai": "",
-  "zenz": "zenzkey_80e2a233dd"
+  "zenz": "zenzkey_80e2a233dd",
+  "dev": { "num1": "94767453646@s.whatsapp.net",
+           "num2": "94776656785@s.whatsapp.net",
+           "emj": "👨‍💻" }
 }
