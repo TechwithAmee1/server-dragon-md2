529f5b6be57ec59ae62db27bb0f660cdc2be820e
៚ᴍʀ.ᴀᴍɪʏᴀ༒ᴏꜰᴄ͢⁸⁵⁴¹༆࿐ <techwithamee@gmail.com>
2024-04-19 20:24:04 +0530

    Add files via upload

--- ext/Logo.js
+++ ext/Logo.js
@@ -1,306 +1,307 @@
 [
   {
-    "no": '1',
+    "no": 1,
     "url": "https://photooxy.com/create-blackpink-style-logo-effects-online-for-free-417.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '2',
+    "no": 2,
     "url": "https://photooxy.com/elegant-3d-neon-dark-metal-text-effect-online-free-416.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '3',
+    "no": 3,
     "url": "https://photooxy.com/online-3d-white-stone-text-effect-utility-411.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '4',
+    "no": 4,
     "url": "https://photooxy.com/logo-and-text-effects/shadow-text-effect-in-the-sky-394.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '5',
+    "no": 5,
     "url": "https://photooxy.com/logo-and-text-effects/quotes-under-fall-leaves-347.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '6',
+    "no": 6,
     "url": "https://photooxy.com/other-design/create-an-easy-smoke-type-effect-390.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '7',
+    "no": 7,
     "url": "https://photooxy.com/logo-and-text-effects/write-text-on-burn-paper-388.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '8',
+    "no": 8,
     "url": "https://photooxy.com/logo-and-text-effects/create-a-picture-of-love-message-377.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '9',
+    "no": 9,
     "url": "https://photooxy.com/logo-and-text-effects/make-quotes-under-grass-376.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '10',
+    "no": 10,
     "url": "https://photooxy.com/logo-and-text-effects/make-tik-tok-text-effect-375.html",
     "dounble_text": true,
     "selection": false
     },
   {
-    "no": '11',
+    "no": 11,
     "url": "https://photooxy.com/logo-and-text-effects/put-your-text-on-a-coffee-cup--174.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '12',
+    "no": 12,
     "url": "https://photooxy.com/logo-and-text-effects/writing-on-wooden-boards-368.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '13',
+    "no": 13,
     "url": "https://photooxy.com/logo-and-text-effects/simple-text-on-the-silk-167.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '14',
+    "no": 14,
     "url": "https://photooxy.com/logo-and-text-effects/make-smoky-neon-glow-effect-343.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '15',
+    "no": 15,
     "url": "https://photooxy.com/logo-and-text-effects/rainbow-shine-text-223.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '16',
+    "no": 16,
     "url": "https://photooxy.com/logo-and-text-effects/army-camouflage-fabric-text-effect-221.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '17',
+    "no": 17,
     "url": "https://photooxy.com/logo-and-text-effects/honey-text-effect-218.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '18',
+    "no": 18,
     "url": "https://photooxy.com/logo-and-text-effects/gradient-avatar-text-effect-207.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '19',
+    "no": 19,
     "url": "https://photooxy.com/logo-and-text-effects/glow-rainbow-effect-generator-201.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '20',
+    "no": 20,
     "url": "https://photooxy.com/logo-and-text-effects/write-stars-text-on-the-night-sky-200.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '21',
+    "no": 21,
     "url": "https://photooxy.com/logo-and-text-effects/realistic-flaming-text-effect-online-197.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '22',
+    "no": 22,
     "url": "https://photooxy.com/logo-and-text-effects/create-embroidery-text-online-191.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '23',
+    "no": 23,
     "url": "https://photooxy.com/other-design/create-3d-text-on-rainbow-online-189.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '24',
+    "no": 24,
     "url": "https://photooxy.com/logo-and-text-effects/text-under-web-matrix-effect-185.html",
     "dounble_text": false,
     "selection": true,
-    "select": "3"
+    "select": 3
     },
   {
-    "no": '25',
+    "no": 25,
     "url": "https://photooxy.com/logo-and-text-effects/butterfly-text-with-reflection-effect-183.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '26',
+    "no": 26,
     "url": "https://photooxy.com/logo-and-text-effects/illuminated-metallic-effect-177.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '27',
+    "no": 27,
     "url": "https://photooxy.com/logo-and-text-effects/create-party-neon-text-effect-161.html",
     "dounble_text": false,
     "selection": true,
-    "select": "5"
+    "select": 5
     },
   {
-    "no": '28',
+    "no": 28,
     "url": "https://photooxy.com/elegant-3d-neon-dark-metal-text-effect-online-free-416.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '29',
+    "no": 29,
     "url": "https://photooxy.com/logo-and-text-effects/make-great-quotes-on-nature-211.html",
     "dounble_text": false,
     "selection": true,
-    "select": "6"
+    "select": 6
     },
   {
-    "no": '30',
+    "no": 30,
     "url": "https://photooxy.com/logo-and-text-effects/write-text-on-the-cup-392.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '31',
+    "no": 31,
     "url": "https://photooxy.com/logo-and-text-effects/royal-look-text-balloon-effect-173.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '32',
+    "no": 32,
     "url": "https://photooxy.com/logo-and-text-effects/create-wallpaper-with-modern-metal-text-179.html",
     "dounble_text": false,
     "selection": true,
-    "select": "6"
+    "select": 6
     },
   {
-    "no": '33',
+    "no": 33,
     "url": "https://photooxy.com/logo-and-text-effects/smoke-typography-text-effect-170.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '34',
+    "no": 34,
     "url": "https://photooxy.com/art-effects/flower-typography-text-effect-164.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '35',
+    "no": 35,
     "url": "https://photooxy.com/logo-and-text-effects/carved-wood-effect-online-171.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '36',
+    "no": 36,
     "url": "https://photooxy.com/logo-and-text-effects/create-harry-potter-text-on-horror-background-178.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '37',
+    "no": 37,
     "url": "https://photooxy.com/logo-and-text-effects/create-vector-nature-typography-355.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '38',
+    "no": 38,
     "url": "https://photooxy.com/league-of-legends/shine-banner-skins-of-league-of-legends-145.html",
     "dounble_text": false,
     "selection": true,
-    "select": "1,1"
+    "select1": 1,
+    "select2": 1
     },
   {
-    "no": '39',
+    "no": 39,
     "url": "https://photooxy.com/other-design/create-dark-metal-text-with-special-logo-160.html",
     "dounble_text": false,
     "selection": true,
-    "select": "11"
+    "select": 11
     },
   {
-    "no": '40',
+    "no": 40,
     "url": "https://photooxy.com/logo-and-text-effects/fur-text-effect-generator-198.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '41',
+    "no": 41,
     "url": "https://photooxy.com/logo-and-text-effects/romantic-messages-for-your-loved-one-391.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '42',
+    "no": 42,
     "url": "https://photooxy.com/logo-and-text-effects/sweet-andy-text-online-168.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '43',
+    "no": 43,
     "url": "https://photooxy.com/logo-and-text-effects/text-on-scary-cemetery-gate-172.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '44',
+    "no": 44,
     "url": "https://photooxy.com/logo-and-text-effects/create-a-wolf-metal-text-effect-365.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '45',
-    "url": "https://photooxy.com/logo-and-text-effects/watermelon-text-style-186.html",
-    "dounble_text": false,
+    "no": 45,
+    "url": "https://photooxy.com/battlegrounds/make-wallpaper-battlegrounds-logo-text-146.html",
+    "dounble_text": true,
     "selection": false
     },
   {
-    "no": '46',
+    "no": 46,
     "url": "https://photooxy.com/logo-and-text-effects/write-art-quote-on-wood-heart-370.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '47',
+    "no": 47,
     "url": "https://photooxy.com/logo-and-text-effects/create-a-3d-glowing-text-effect-220.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '48',
+    "no": 48,
     "url": "https://photooxy.com/other-design/create-metallic-text-glow-online-188.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '49',
+    "no": 49,
     "url": "https://photooxy.com/logo-and-text-effects/text-under-flower-165.html",
     "dounble_text": false,
     "selection": false
     },
   {
-    "no": '50',
+    "no": 50,
     "url": "https://photooxy.com/logo-and-text-effects/gradient-avatar-text-effect-207.html",
     "dounble_text": false,
     "selection": false
